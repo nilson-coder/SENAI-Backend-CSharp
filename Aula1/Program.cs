3698615b26c8d30aec678dec123eda90eb83466d
FIC <FIC@info.local>
2019-09-13 09:35:45 -0300

    Arquivos incluidos

---
+++ Aula1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // somente declaramos, sem atribuir valor
+            int num1;
+
+            // declaramos atribuindo um valor
+            int num2 = 5;
+
+            // declaramos diversas variaveis do mesmo tipo
+            int num3, num4, num5 = 7;
+
+            // variavel do tipo decimal (real)
+            float media;
+
+            // variavel do tipo logico (booleana)
+            bool resultado = false;
+
+            Console.WriteLine(num2 + num5);
+
+        }
+    }
+}
