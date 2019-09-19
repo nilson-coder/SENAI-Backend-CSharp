b4629dc45a71e84d6b756b2bf711729940cca9a0
FIC <FIC@info.local>
2019-09-19 12:24:32 -0300

    Laço condicional - do / while

---
+++ Aula3-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumuladora1 = 0, nota = 1, cont = 1;
+
+            // Laço condicional - do / While
+            do{
+                Console.WriteLine("Digite nota do aluno: ");
+                nota = int.Parse(Console.ReadLine());
+
+                if(nota % 3 == 0){
+                    acumuladora1 = acumuladora1 + nota;
+                }
+                cont ++;
+
+            }while(cont <= 20);
+
+                Console.WriteLine(acumuladora1);
+
+        }
+    }
+}
