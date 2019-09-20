f44c831828bda4d61ffd0c533e8b0ee691459c03
FIC <FIC@info.local>
2019-09-20 16:02:12 -0300

    aula4

--- Aula4/exercícios/Ex/Program.cs
+++ Aula4/exercícios/Ex/Program.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 
-namespace Aula4
+namespace Ex
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
             double[] media = new double[10];
 
             for(int i = 0; i <= 9; i++){
 
-                Console.Write("Digite uma media: ");
+                Console.Write("Digite a posições: ");
                 media[i] = double.Parse(Console.ReadLine());
-
             }
 
             Console.WriteLine(media[10]);
             Console.WriteLine();
 
-            for(int i = 0; i <= media.Length -1; i++){
+
+            for(int i = 0; i <= media.Length -1 ; i++){
 
                 Console.WriteLine("Média :" + media[i]);
 
+            
             }
-
         }
     }
 }
---
+++ Aula4/exercícios/Ex2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Faça um algoritmo que leia os valores de dois vetores de 10 posições gere um terceiro
+            //vetor de 20 posições que seja a união dos dois primeiros.
+            
+            string[] valo1 = new string[10];
+            string[] valo2 = new string[10];
+            string[] valo3 = new string[20];
+            
+            for(int i = 0; i <= 9; i++){
+                Console.WriteLine("Digite o valor: ");
+                valo1[i] = Console.ReadLine();
+            }
+
+            for(int i = 0; i <= 9; i++){
+                Console.WriteLine("Digite o valor: ");
+                valo2[i] = Console.ReadLine();
+            }
+
+            for(int i = 0; i <= 9; i++){
+                valo3[i] = valo1[i];
+                valo3[i+10] = valo2[i];
+
+            }
+
+            for(int i = 0; i <= 20; i++){
+                Console.WriteLine(valo3[i]);
+            }
+
+        }
+    }
+}
