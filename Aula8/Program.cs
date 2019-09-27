9fa1269031969fc93f70db0e040147a642b494a3
FIC <FIC@info.local>
2019-09-27 13:14:04 -0300

    Laço FOREACH

---
+++ Aula8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] v1 = {"A", "B", "C", "D", "E"};
+
+            // Laço FOREACH (para cada)
+            foreach(string letra in v1){
+                Console.WriteLine(letra);
+            }
+            
+        }
+    }
+}
