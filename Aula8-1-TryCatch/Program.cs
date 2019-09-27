fb7cbe58238508e08e6f3ab94ddd0cf4d9774683
FIC <FIC@info.local>
2019-09-27 13:15:21 -0300

    TryCatch

---
+++ Aula8-1-TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            
+            try{
+
+                // Escrevemos um bloco de código
+                Console.WriteLine("Digite um numero");
+                numero = int.Parse(Console.ReadLine());
+
+            }catch(Exception ex){
+
+                // Mostramos a exceção da regra erro gerado na aplicação
+                Console.WriteLine("Ops :( Erro: " + ex);
+            }
+
+        }
+    }
+}
