963d7dd947c5b81a63e6cd895b2e8bdc9c1a0ce1
FIC <FIC@info.local>
2019-09-30 08:57:14 -0300

    Função

---
+++ Aula8-Exercícios/Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Console.WriteLine("o dia da semana é ");
+        DiaDaSemana();
+
+        }
+        /// <summary>
+        /// DATA e HORA
+        /// </summary>
+        static void DiaDaSemana(){
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+        }
+    }
+}
---
+++ Aula8-Exercícios/Ex3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            System.Console.WriteLine(info());
+
+            string nome, sobrenome;
+            System.Console.WriteLine("Digite nome ");
+            nome = Console.ReadLine();
+            System.Console.WriteLine("Digite sobrenome ");
+            sobrenome = Console.ReadLine(   );
+
+            string idade, peso, altura;
+            System.Console.WriteLine("Digite idade, peso, altura");
+            idade = (Console.ReadLine());
+            peso = (Console.ReadLine());
+            altura = (Console.ReadLine());
+
+            System.Console.WriteLine(info(nome, sobrenome));
+            System.Console.WriteLine(info(idade, peso, altura));          
+            
+        }
+
+        static string info(){
+            return "razão social da escola Senai";
+        }
+             
+        static string info(string nome, string sobrenome){
+            return nome + sobrenome;
+        }
+
+        static string  info(string idade, string peso, string altura){
+            return ("sua idade " + idade + "\n" + "seu peso " +  peso + "\n" + "sua altura " + altura);
+        }
+    }
+}
