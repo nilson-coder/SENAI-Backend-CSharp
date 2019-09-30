e4e85efa7d2880eda6f293ed7ce0cbf5051fc645
FIC <FIC@info.local>
2019-09-30 08:51:47 -0300

    Ex de Função

--- Aula8-Ex/Ex5/Program.cs
+++ Aula8-Ex/Ex5/Program.cs
@@ -14,26 +14,7 @@ namespace Ex5
 
         static double CalculaMedia( double[] valores){
 
-            int num = 0;
-            string sair = "";
-            int num2 = 0;
-
-            while(sair != "sim"){
-                try{
-                Console.Write("Digite um número: ");
-                num = int.Parse(Console.ReadLine());
-
-                for(int i = 0; i <= num.ToString().Length; i++){
-                    num2 = num2 + num;
-                }
-
-                Console.Write("Deseja sair ? sim/não ");  
-                sair = Console.ReadLine().ToLower(); 
-                }catch{
-                Console.WriteLine("erro!");
-            }
-            }
-            return num2;
+    
         }
 
     }
